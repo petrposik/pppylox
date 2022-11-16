4bced6be44c520750ac05ae860e59c4fe5af6473
Petr Pošík <petr.posik@gmail.com>
2022-11-16 08:36:25 +0100

    Add nested environments (Sec. 8.5.1)

--- pylox/environment.py
+++ pylox/environment.py
@@ -3,8 +3,9 @@ from .errors import LoxRuntimeError
 
 
 class Environment:
-    def __init__(self):
+    def __init__(self, enclosing: "Environment" = None):
         self.values = {}
+        self.enclosing = enclosing
 
     def define(self, name: str, value):
         self.values[name] = value
@@ -13,9 +14,13 @@ class Environment:
         if name.lexeme in self.values:
             self.values[name.lexeme] = value
             return None
+        if self.enclosing:
+            self.enclosing.assign(name, value)
         raise LoxRuntimeError(name, f"Undefined variable '{name.lexeme}'.")
 
     def get(self, name: Token):
         if name.lexeme in self.values:
             return self.values[name.lexeme]
+        if self.enclosing:
+            return self.enclosing.get(name)
         raise LoxRuntimeError(name, f"Undefined variable '{name.lexeme}'.")
